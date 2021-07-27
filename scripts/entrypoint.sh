d2884ab9a3470c00e467e027951ae02d06ba78c7
Kim Taehwan <31404686+ktcar214@users.noreply.github.com>
2021-07-27 11:52:02 +0900

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,7 @@ if [ -d "/github" ]; then
 sudo chown -R build /github/workspace /github/home
 fi
 
+pwd
 sudo pacman -Sy
 namcap PKGBUILD
 sudo pacman -Syyu --noconfirm && makepkg -fC --syncdeps --noconfirm
